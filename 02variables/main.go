9bf50e569e244b80f002746803a031eace151285
tahsinAmin <amin1996bangladesh@gmail.com>
2024-04-08 10:37:59 +0600

    # Learn - Golang

---
+++ 02variables/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	var username string = "Hitesh"
+	fmt.Println("Variables")
+	fmt.Printf("Variables is a of type: %T \n", username)
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+## Production ready file (Before you write any code, you have to write)
+'go mod init hello'
+
+## Run
+
+'go run <file_name>.go'
