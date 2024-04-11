66a0f696698bcf0abc2dc85b136f5deb8efcfe4f
tahsinAmin <muhammadtahsinamin@gmail.com>
2024-04-11 17:32:02 +0600

    Completed time

---
+++ 05mytime/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("Welcome to time study of golang")
+
+	presentTime := time.Now()
+	fmt.Println(presentTime)
+	fmt.Println(presentTime.Format("01-02-2006 Monday 15:04:05"))
+
+	createdDate := time.Date(2024, time.May, 9, 1, 1, 45, 0, time.UTC)
+	fmt.Println("\n")
+	fmt.Println(createdDate)
+	fmt.Println(createdDate.Format("01-02-2006 Monday 15:04:05"))
+}
